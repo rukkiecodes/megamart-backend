b8206a5903a46b52133edcf28e363154b1800d47
rukkiecodes <rukkiecodes@gmail.com>
2023-02-23 22:36:24 +0100

    auth works

--- models/user.js
+++ models/user.js
@@ -7,12 +7,12 @@ const userSchema = new mongoose.Schema({
   rating: { type: Number },
   name: { type: String, required: true },
   email: { type: String, required: true },
-  phone: { type: String, required: true },
-  gender: { type: String, required: true },
-  state: { type: String, required: true },
-  lga: { type: String, required: true },
-  specialty: { type: String, required: true },
   password: { type: String, required: true },
+  phone: { type: String },
+  gender: { type: String },
+  state: { type: String },
+  lga: { type: String },
+  specialty: { type: String },
   description: { type: String },
   verified: Boolean
 }, { timestamps: true })
